9171760b83e4d6c0cabb6b7beb62b653b9a7bd17
Navin Kumar <naveen569652@gmail.com>
2025-06-21 21:28:54 +0530

    Added CBV Employee and perform API CRUD

--- django_rest/django_rest/api/serializers.py
+++ django_rest/django_rest/api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from students.models import students
+from employee.models import Employee
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = students
+        fields = '__all__'
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
         fields = '__all__'
--- django_rest/django_rest/api/urls.py
+++ django_rest/django_rest/api/urls.py
@@ -3,4 +3,8 @@ from . import views
 urlpatterns = [
     path('students/', views.list_students,name='all_students'),
     path('students/<int:pk>/', views.get_student,name='single_student'),
+    
+    path('employees/',views.Employees.as_view()),
+    path('employees/<int:id>/',views.EmployeeDetail.as_view())
+    
 ]
