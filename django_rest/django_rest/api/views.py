b8f63cade835c4f23381d5fbf84134e7ecc3baad
Navin Kumar <naveen569652@gmail.com>
2025-06-22 23:43:15 +0530

    Converted the Mixins to use Generics for Cloth Model

--- django_rest/django_rest/api/views.py
+++ django_rest/django_rest/api/views.py
@@ -56,42 +56,45 @@ def get_student(request, pk):
         single_student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-# class Employees(APIView):
-#     def get(self, request):
-#         employees = Employee.objects.all()
-#         serializer = EmployeeSerializer(employees,many= True)
-#         return Response(serializer.data,status=status.HTTP_200_OK)
+"""
+# Class based approach
+class Employees(APIView):
+    def get(self, request):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees,many= True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     
-#     def post(self,request):
-#         serializer = EmployeeSerializer(data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-# class EmployeeDetail(APIView):
-#     def get_object(self,id):
-#         try:
-#             return Employee.objects.get(id = id)
-#         except Employee.DoesNotExist:
-#             raise Http404
+    def post(self,request):
+        serializer = EmployeeSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+class EmployeeDetail(APIView):
+    def get_object(self,id):
+        try:
+            return Employee.objects.get(id = id)
+        except Employee.DoesNotExist:
+            raise Http404
         
-#     def get(self,request,id):
-#         employee = self.get_object(id)
-#         serializer = EmployeeSerializer(employee)
-#         return Response(serializer.data,status=status.HTTP_200_OK)
+    def get(self,request,id):
+        employee = self.get_object(id)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     
-#     def put(self,request,id):
-#         employee = Employee.objects.get(id = id)
-#         serializer = EmployeeSerializer(employee,data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_200_OK)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-#     def delete(self,request,id):
-#         employee = self.get_object(id)
-#         employee.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self,request,id):
+        employee = Employee.objects.get(id = id)
+        serializer = EmployeeSerializer(employee,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def delete(self,request,id):
+        employee = self.get_object(id)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
+"""
 class Employees(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
@@ -146,44 +149,49 @@ def get_update_delete(request,pk):
         single_book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
-# class Clothes(APIView):
+"""
+# Class Based approach
+class Clothes(APIView):
     
-#     def get(self,request):
-#         all_clothes = Cloth.objects.all()
-#         serializer = ClothSerializer(all_clothes,many= True)
-#         return Response(serializer.data,status=status.HTTP_200_OK)
+    def get(self,request):
+        all_clothes = Cloth.objects.all()
+        serializer = ClothSerializer(all_clothes,many= True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     
-#     def post(self,request):
-#         serializer = ClothSerializer(data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def post(self,request):
+        serializer = ClothSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-# class ClothDetail(APIView):
-#     def get_object(self,pk):
-#         try:
-#             return Cloth.objects.get(pk = pk)
-#         except Cloth.DoesNotExist:
-#             return Response(status=status.HTTP_400_BAD_REQUEST)
-#     def get(self,request,pk):
-#         single_cloth = self.get_object(pk)
-#         serializer = ClothSerializer(single_cloth)
-#         return Response(serializer.data,status=status.HTTP_200_OK)
+class ClothDetail(APIView):
+    def get_object(self,pk):
+        try:
+            return Cloth.objects.get(pk = pk)
+        except Cloth.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    def get(self,request,pk):
+        single_cloth = self.get_object(pk)
+        serializer = ClothSerializer(single_cloth)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     
-#     def put(self,request,pk):
-#         update_cloth = self.get_object(pk)
-#         serializer = ClothSerializer(update_cloth,data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_200_OK)
-#         return Response(status=status.HTTP_400_BAD_REQUEST)
+    def put(self,request,pk):
+        update_cloth = self.get_object(pk)
+        serializer = ClothSerializer(update_cloth,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
     
-#     def delete(self,request,pk):
-#         delete_cloth = self.get_object(pk)
-#         delete_cloth.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self,request,pk):
+        delete_cloth = self.get_object(pk)
+        delete_cloth.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+"""
 
+"""
+# Mixins start
 class Clothes(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
     queryset = Cloth.objects.all()
     serializer_class = ClothSerializer
@@ -203,3 +211,25 @@ class ClothDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.Destr
     
     def delete(self,request,pk):
         return self.destroy(request,pk)
+
+# Mixins End
+"""
+# Generics start
+"""
+-> ListAPIView: Provides a read-only endpoint to list all objects (GET).
+-> CreateAPIView: Provides an endpoint to create a new object (POST).
+-> RetrieveAPIView: Provides a read-only endpoint to retrieve a single object (GET with pk).
+-> UpdateAPIView: Provides an endpoint to update an existing object (PUT or PATCH with pk).
+-> DestroyAPIView: Provides an endpoint to delete an object (DELETE with pk).
+-> ListCreateAPIView: Combines listing all objects and creating new ones (GET + POST).
+-> RetrieveUpdateDestroyAPIView: Combines retrieve, update, and delete for a single object (GET, PUT, PATCH, DELETE with pk).
+
+"""
+class Clothes(generics.ListCreateAPIView,generics.DestroyAPIView):
+    queryset = Cloth.objects.all()
+    serializer_class = ClothSerializer
+
+class ClothDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cloth.objects.all()
+    serializer_class = ClothSerializer
+    lookup_field = 'pk'
